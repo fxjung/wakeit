5a8b33bb51be69c0aebe25c6a230c3a2863dcdff
Felix Jung <felix.jung@wilhelm-gym.net>
2010-12-26 01:03:05 +0100

    Improved usability of config file.
    Removed those silly numbers, used between some tokens and the "=" to enumerate the multiple values.

--- ConfigFile.cpp
+++ ConfigFile.cpp
@@ -60,18 +60,18 @@ string* ConfigFile::read_multi_string(const string token) {
 	string* multi_string = new string[multi_token_max];
 	for(int i = 0; i < multi_token_max; i++) multi_string[i] = "";
 	if(!config) {cout << "Error: Cannot open file " << file << endl; return multi_string;}
+	int number = 0;
 
 	for (int i = 0; !config.eof(); i++) {
     	char buf[100] = {0};
     	string first;
-    	string number;
    		string second;
     	string content;
 
     	config.getline(buf, sizeof(buf));
     	istringstream istr(string(buf), ios_base::out);
-    	istr >> first >> number >> second >> content;
-   		if(first == token && atoi(number.c_str()) > -1 && atoi(number.c_str()) < multi_token_max) multi_string[atoi(number.c_str())] = content;
+    	istr >> first >> second >> content;
+   		if(first == token && number < multi_token_max) {multi_string[number] = content; number++;}
  	}
  	return multi_string;
 }
@@ -86,22 +86,23 @@ int** ConfigFile::read_multi_date(const string token) {
 		}
 	}
 	if(!config) {cout << "Error: Cannot open file " << file << endl; return multi_date;}
+	int number = 0;
 
 	for (int i = 0; !config.eof(); i++) {
     	char buf[100] = {0};
     	string first;
-    	string number;
    		string second;
     	string content[6];
     	string third;
 
     	config.getline(buf, sizeof(buf));
     	istringstream istr(string(buf), ios_base::out);
-    	istr >> first >> number >> second >> content[0] >> content[1] >> content[2] >> third >> content[3] >> content[4] >> content[5];
-   		if(first == token && atoi(number.c_str()) > -1 && atoi(number.c_str()) < multi_token_max) {
+    	istr >> first >> second >> content[0] >> content[1] >> content[2] >> third >> content[3] >> content[4] >> content[5];
+   		if(first == token && number < multi_token_max) {
    			for(int j = 0; j < 6; j++) {
-   				multi_date[atoi(number.c_str())][j] = atoi(content[j].c_str());
+   				multi_date[number][j] = atoi(content[j].c_str());
    			}
+   			number++;
    		}
  	}
  	return multi_date;
